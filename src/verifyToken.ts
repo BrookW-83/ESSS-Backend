92175d9522255a268aae358434e6dd8bf2983ea4
Nathnael Teshome <nathnaelteshome10@gmail.com>
2024-06-01 21:53:45 +0300

    chore: Remover authentication in verifyToken

--- src/verifyToken.ts
+++ src/verifyToken.ts
@@ -1,23 +1,9 @@
-import jwt from "jsonwebtoken";
-import { createError } from "./error";
 import { NextFunction, Request, Response } from "express";
 
-interface CustomRequest extends Request {
-  user?: {
-    id: string;
-  };
-}
 export const verifyToken = (
-  req: CustomRequest,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.cookies.access_token;
-  if (!token) return next(createError("You need to login", 401));
-
-  jwt.verify(token, process.env.SECRET_KEY, (err: any, user: any) => {
-    if (err) return next(createError("Token is not valid!", 403));
-    req.user = user;
-    next();
-  });
+  next();
 };
